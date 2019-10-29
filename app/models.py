c804dd2af5d740328e0edb4488a792a35be5f5d5
xiaoluo <you@example.com>
2019-10-29 11:14:19 -0400

    add file

--- app/models.py
+++ app/models.py
@@ -4,11 +4,23 @@ from app import db, app
 from passlib.apps import custom_app_context
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, SignatureExpired, BadSignature
 
+
+user_role = db.Table('user_role',db.Column('user_id', db.Integer, db.ForeignKey('user.id')),db.Column('role_id', db.Integer, db.ForeignKey('role.id')))
+
+
+
+
 class User(db.Model):
-    __tablename__ =  'users'
+    __tablename__ =  'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), index=True)
+    name = db.Column(db.String(255))    
     password = db.Column(db.String(128))
+    email = db.Column(db.String(255))
+    sex = db.Column(db.Integer)
+    addr = db.Column(db.String(255))                                                                                                                   
+    avatar = db.Column(db.TEXT)
+    roles = db.relationship('Role', secondary=user_role, back_populates='users')
 
     def hash_password(self, password):
         self.password = custom_app_context.encrypt(password)
@@ -32,3 +44,12 @@ class User(db.Model):
             return None # invalid token
         user = User.query.get(data['id'])
         return user
+
+class Role(db.Model):
+    __tablename__ = 'role'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(64), unique=True)
+    comment = db.Column(db.TEXT)
+    users = db.relationship('User', secondary=user_role, back_populates='roles') 
+    def __repr__(self):
+        return 'Role:%s'% self.name
--- app/user.py
+++ app/user.py
@@ -1,15 +1,29 @@
 from flask_restful import Resource
 from app import api,auth
-from flask import jsonify,g
+from flask import jsonify,g,request
 from app.models import *
 from utils import *
-
+import json
 
 class UserAPI(Resource):
     decorators = [auth.login_required]
     def get(self):
-        users = User.query.all()
-        result=process_result(users,[])
-        return jsonify(result)
+        name = request.args.get('name')
+        page = request.args.get('page',type=int)
+        if not name:
+            pagination = User.query.order_by(User.id.desc()).paginate(page,per_page=10,error_out=False)
+            db.session.close()
+            ret = pagination.items
+            result = process_result(ret, [])
+            data = {'users':result}  
+            data['total']=pagination.total	
+            return jsonify(data)
+        else:
+            pagination = User.query.filter(User.name.like('%' + name + '%')).paginate(page, per_page=10, error_out=False)
+            ret = pagination.items	
+            result = process_result(ret, [])
+            data = {'users':result}
+            data['total']=pagination.total
+            return jsonify(data) 
 
 api.add_resource(UserAPI, '/devops/api/v1.0/user', endpoint = 'user')
